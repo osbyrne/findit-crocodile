b335a9e059b71cafe19d2bbfa6a9a6d2812e50d4
Oscar <osbyrne@skiff.com>
2023-12-26 09:28:18 +0100

    trying CSS styles

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,12 +2,33 @@
     import { Button, Label, Input } from 'flowbite-svelte';
 </script>
 
+<style>
+    h1 {
+        text-align: center;
+    }
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    form > * {
+        margin: 10px;
+    }
+    body {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        margin: auto;
+    }
+</style>
+
 <main>
     <body>
         <h1>Login</h1>
         <form action="?/login" method="POST">
-            <Label for="email"> Email </Label>
-            <Input type="text" name="email" required placeholder="name@flowbite.com" />
+            <label for="email"> Email </labe>
+            <input type="text" name="email" required placeholder="name@flowbite.com" />
 
             <Label for="password"> Password </Label>
             <Input type="password" name="password" required placeholder="R$N2KQ8eU83%X*UqZBn*" />
